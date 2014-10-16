7b97a7ae8626acfb29c0cefcdae9fea69f28f987
Daniel Abad <abadabanades@gmail.com>
2014-10-16 11:58:31 +0200

    cleaning and structuring code

--- HGraph.js
+++ HGraph.js
@@ -12,13 +12,15 @@ function HGraph(){
 //Method to add a Vertex to the Graph
 HGraph.prototype.addVertex = function(son,parent){
   this.vertices.push(son);
-  parent.links.push(son);
+  parent.linksTo.push(son);
+  son.linksFrom.push(parent);
 }
 
 //Constructor for Vertices, need for an id to uniquely identify
 function Vertex(id){
   this.id = id;
-  this.links = [];
+  this.linksTo = [];
+  this.linksFrom = [];
 }
 
 //Method to add an Edge to the Graph
@@ -29,10 +31,49 @@ function Vertex(id){
   
 //}
 
-
-
 //Constructor for Edges, simply a link between two vertices
 //function Edge(vertex1,vertex2){
   //this.vertex1 = vertex1;
   //this.vertex2 = vertex2;
 //}
+
+//Function that creates a random Graph (to be modified when DB available)
+function createRandomGraph(maxChildren,minChildren,depthLevel) {
+
+  //default parameters
+  var maxChildren = maxChildren || 4;
+  var minChildren = minChildren || 2;
+  var depthLevel = depthLevel || 4;
+
+  //create the HGraph
+  var graph = new HGraph();
+
+  //create first vertex and add it to the HGraph
+  var id = 0;
+  var vertex = new Vertex(id);
+  graph.vertices.push(vertex);
+  //graph.vertices[id].title="soy el nodo 0"; //to add some properties
+  id++;
+
+  //add random number of children to the node
+  var numChildren = Math.floor(Math.random() * (maxChildren-minChildren+1) + minChildren);
+  for (var i=0 ; i<numChildren ; i++){
+    addChild(vertex,depthLevel-1);
+  }
+
+  //add a child to the parent
+  function addChild(parent,prof){
+    if (prof>=0){
+      var vertix = new Vertex(id);
+      graph.addVertex(vertix,parent);
+      //graph.vertices[id].title="soy el nodo "+id;  //to add some properties
+      id++;
+      var numiChildren = Math.floor(Math.random() * (maxChildren-minChildren+1) + minChildren);
+      for (var i=0 ; i<numiChildren ; i++){
+        addChild(vertix,prof-1);                    
+      }
+    }
+  }
+  //console.log("Creating hypergraph with "+graph.vertices+" nodes");  
+  return graph;
+}
