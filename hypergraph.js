4bf6b2bca57ca2291acaf37c30566a1296be12d0
Daniel Abad <abadabanades@gmail.com>
2014-10-27 15:42:06 +0100

    updated to ThreeJS R69 and restructuration of code (HGraph.js well defined

--- hypergraph.js
+++ hypergraph.js
@@ -4,19 +4,66 @@
   HGraph Structure G = (V,E)
  */
 
+/*
+ * HGraph graph with tree structure
+ */
 function HGraph(){
   this.vertices = [];
-  //this.edges = [];
 }
 
-//Method to add a Vertex to the Graph
+/* Function to add a Vertex to the HGraph
+ *
+ * @param son vertex linked from the parent
+ * @param parent vertex linking the son
+ */
 HGraph.prototype.addVertex = function(son,parent){
   this.vertices.push(son);
   parent.linksTo.push(son);
   son.linksFrom.push(parent);
 }
 
-//Constructor for Vertices, need for an id to uniquely identify
+/*
+ * Function that sets the property processed to false for every vertex
+ */
+HGraph.prototype.resetProcessed = function(){
+  for (var i = 0 ; i < this.vertices.length ; i++) {  
+    this.vertices[i].processed=false;
+  }
+}
+
+/*
+ * Function to find out the number of nodes
+ *
+ * @return this.vertices.length number of nodes
+ */
+HGraph.prototype.numberOfNodes = function(){
+  return this.vertices.length;
+}
+
+/* Function that evaluates the not processed (drawn) links for vertex
+ *
+ * @param id node for which we want to get the links
+ * @return links array with the not processed links
+ */
+HGraph.prototype.linksNotProcessed = function(id){
+  var links = [];
+  for (var i = 0 ; i < this.vertices[id].linksTo.length ; i++) {
+      if (! this.vertices[id].linksTo[i].processed){
+          links.push(this.vertices[id].linksTo[i]);
+      }
+  }
+  for (var i = 0 ; i < this.vertices[id].linksFrom.length ; i++) {
+      if (! this.vertices[id].linksFrom[i].processed){
+          links.push(this.vertices[id].linksFrom[i]);
+      }
+  }
+  return links;
+}
+
+/* Constructor for Vertices, uniquely identifiable by id
+ *
+ * @param id identifier of the node
+ */
 function Vertex(id){
   this.id = id;
   this.linksTo = [];
@@ -24,29 +71,14 @@ function Vertex(id){
   this.processed = false;
 }
 
-HGraph.prototype.preprocessing = function(){
-  for (var i = 0 ; i < this.vertices.length ; i++) {  
-    this.vertices[i].processed=false;
-  }
-}
-
-//Method to add an Edge to the Graph
-//HGraph.prototype.addEdge = function(son,parent){
-  //var edge = new Edge(son,parent);
-  //this.edges.push(edge);
-  //vertex1.links.push(vertex2);
-  
-//}
-
-//Constructor for Edges, simply a link between two vertices
-//function Edge(vertex1,vertex2){
-  //this.vertex1 = vertex1;
-  //this.vertex2 = vertex2;
-//}
-
-//Function that creates a random Graph (to be modified when DB available)
+/* Function that creates a random Graph with
+ *
+ * @param maxChildren number of maximum children per node
+ * @param minChildren number of minumum children per node
+ * @param depthLevel maximum number of levels from the center
+ * @return HGraph graph desired graph
+ */
 function createRandomGraph(maxChildren,minChildren,depthLevel) {
-
   //default parameters
   var maxChildren = maxChildren || 4;
   var minChildren = minChildren || 2;
@@ -68,16 +100,20 @@ function createRandomGraph(maxChildren,minChildren,depthLevel) {
     addChild(vertex,depthLevel-1);
   }
 
-  //add a child to the parent
-  function addChild(parent,prof){
-    if (prof>=0){
-      var vertix = new Vertex(id);
-      graph.addVertex(vertix,parent);
+  /* Function that adds a child to a parent
+   *
+   * @param parent Vertex that has this child
+   * @param depth level of depth where we are
+   */
+  function addChild(parent,depth){
+    if (depth>=0){
+      var vertex = new Vertex(id);
+      graph.addVertex(vertex,parent);
       graph.vertices[id].id=id;
       id++;
-      var numiChildren = Math.floor(Math.random() * (maxChildren-minChildren+1) + minChildren);
-      for (var i=0 ; i<numiChildren ; i++){
-        addChild(vertix,prof-1);                    
+      var numChildren = Math.floor(Math.random() * (maxChildren-minChildren+1) + minChildren);
+      for (var i=0 ; i<numChildren ; i++){
+        addChild(vertex,depth-1);                    
       }
     }
   }
