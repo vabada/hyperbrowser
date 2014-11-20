743e2a0cb711e8347c1b3055ef1fb5f9012540b5
Daniel Abad <abadabanades@gmail.com>
2014-11-20 18:17:46 +0100

    creating log file locally, to upload to heroku

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-var express = require('express')
-var bodyParser = require('body-parser');
-var fs = require('fs');
+var express    = require('express')
+  , http       = require('http')
+  , path       = require('path')
+  , db         = require('./models')
+  , fs         = require('fs')
+  , bodyParser = require('body-parser')
+  , pg         = require('pg');
 
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
+// all environments
+app.set('port', (process.env.PORT || 5000));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
-
 
 app.get('/', function(req, res) {
     res.redirect('javascript.html');
@@ -31,6 +37,16 @@ app.get('/shaders', function(req, res) {
 
 //stats handler (copying them to a file)
 app.post('/statsHandler', function(req, res) {
+
+    //write to database
+    pg.connect(process.env.DATABASE_URL, function(err, client) {
+      //var query = client.query('SELECT * FROM your_table');
+
+      //query.on('row', function(row) {
+      //console.log(JSON.stringify(row));
+      });
+    });
+
     console.log("Receiving stats:");
     console.log(req.body);
     fs.appendFile('stats.log', JSON.stringify(req.body)+"\n", function (err) {
@@ -40,6 +56,16 @@ app.post('/statsHandler', function(req, res) {
     res.status(200).end();
 });
 
-app.listen(app.get('port'), function() {
+/*app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
+})*/
+
+db.sequelize.sync().complete(function(err) {
+  if (err) {
+    throw err[0]
+  } else {
+    http.createServer(app).listen(app.get('port'), function(){
+      console.log('Express server listening on port ' + app.get('port'))
+    })
+  }
 })
